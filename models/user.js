3cffff0fcd2d9d715355350c67e208ae1bb80a35
ArdianChandra <sensenkac@gmail.com>
2021-03-19 12:36:19 +0700

    user migration and user model validation

--- models/user.js
+++ models/user.js
@@ -2,6 +2,9 @@
 const {
   Model
 } = require('sequelize');
+
+const { hashPassword } = require('../helper/bcrypt')
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -11,17 +14,69 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      User.hasMany(models.Field)
     }
   };
   User.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    username: DataTypes.STRING,
-    password: DataTypes.STRING
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {msg: "First Name cannot be NULL"},
+        notEmpty: {msg: "First Name cannot be Empty"}
+      }
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {msg: "Last Name cannot be NULL"}
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {msg: "Email cannot be NULL"},
+        notEmpty: {msg: "Email cannot be Empty"},
+        isEmail: {msg: "Invalid Email Format"}
+      }
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {msg: "Username cannot be NULL"},
+        notEmpty: {msg: "Username cannot be Empty"},
+        len: {
+          args: 4,
+          msg: 'Username must contain at leats 4 characters'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {msg: "Password cannot be NULL"},
+        notEmpty: {msg: "Password cannot be Empty"},
+        len: {
+          args: 6,
+          msg: 'Password must contain at leats 6 characters'
+        }
+      }
+    },
   }, {
     sequelize,
     modelName: 'User',
+    hooks: {
+      beforeCreate(user) {
+        user.password = hashPassword(user.password)
+        if(user.lastName === null){
+          user.lastName = ''
+        }
+      }
+    }
   });
   return User;
 };
