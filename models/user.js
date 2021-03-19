3a23345f1dca0dbc9f80080651d3c4c758bc00ae
rorodaniela <daniela.putri28@gmail.com>
2021-03-19 19:40:34 +0700

    add userId to fields

--- models/user.js
+++ models/user.js
@@ -14,7 +14,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      User.hasMany(models.Field)
+      User.hasMany(models.Field, { foreignKey: 'userId' })
     }
   };
   User.init({
