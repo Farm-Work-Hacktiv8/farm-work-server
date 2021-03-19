fe1ebf421ecbfb77cde869fd820cd865979d65bb
ArdianChandra <sensenkac@gmail.com>
2021-03-19 13:03:54 +0700

    initial Routing

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,30 @@
 const router = require('express').Router()
+const userRoute = require('./user-route')
+const fieldRoute = require('./field-route')
+const plantRoute = require('./plant-route')
 
-router.get('/', (req, res) => {
-    res.send('Hello from the other side')
-})
+router.use('/', userRoute)
+router.use('/fields', fieldRoute)
+router.use('/plants', plantRoute)
 
 module.exports = router
+
+/**
+ * User
+- POST/login
+- POST/register
+
+Fields
+
+- GET/fields
+- POST/fields
+- PUT/fields/:id
+- DELETE/fields/:id
+
+Plants
+
+- GET/plants
+- POST/plants
+- PUT/plants/:id
+- DELETE/plants/:id
+ */
