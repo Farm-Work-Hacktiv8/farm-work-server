4e298391f0562355da058351da979e7491634a4e
mrdanang <mrdanang4108@gmail.com>
2021-03-21 11:00:03 +0700

    add antares connection to server

--- app.js
+++ app.js
@@ -2,17 +2,27 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const errorHandling = require('./middleware/errorHandling')
+const server = require('http').createServer(app)
 
 const router = require("./routes");
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+let data 
+app.post('/monitor', function(req, res, next) {
+    const response = (JSON.parse(req.body['m2m:sgn']['m2m:nev']['m2m:rep']['m2m:cin'].con))
+    console.log(response, '<<<<')
+    data = response
+    res.send(response);
+});
+app.get('/data', (req, res) => {
+    res.send(data)
+})
 app.use(router);
 app.use(errorHandling);
 
-module.exports = app;
+module.exports = {app, server};
 
 
 // untuk cek coverage testing jest
--- bin/http.js
+++ bin/http.js
@@ -1,6 +1,9 @@
-const app = require("../app");
+const {app, server} = require("../app");
 const port = process.env.PORT || 3000;
 
+server.listen(9000, function() {
+    console.log('App listening on port 9000');
+});
 app.listen(port, () => {
     console.log(`Farm Work server listen on port ${port}`);
 });
