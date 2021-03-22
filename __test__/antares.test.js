fb5226322242a58c430fe96e40abb50d99a0f2d4
mrdanang <mrdanang4108@gmail.com>
2021-03-22 15:52:07 +0700

    update TDD

--- __test__/antares.test.js
+++ __test__/antares.test.js
@@ -50,9 +50,33 @@ afterAll((done) => {
 // ========================== GET data from Antares ==========================
 describe('POST /monitor', function() {
   it('hit antares server', function(done) {
+    let body = {
+      'm2m:sgn': {
+        'm2m:nev': {
+          'm2m:rep': {
+            'm2m:cin': {
+              rn: 'cin_vjKniM7GQRG4SJrT',
+              ty: 4,
+              ri: '/antares-cse/cin-vjKniM7GQRG4SJrT',
+              pi: '/antares-cse/cnt-1vveUEpIR9aXEwK5',
+              ct: '20210322T143817',
+              lt: '20210322T143817',
+              st: 0,
+              cnf: 'text/plain:0',
+              cs: 143,
+              con: '{"temperature":29.4,"humidity":56,"wind_speed":6.306306,"rain_level":1.287554,"latitude":"-6.8718189","longitude":"107.5872477","moisture":101}'
+            }
+          },
+          'm2m:rss': 1
+        },
+        'm2m:sud': false,
+        'm2m:sur': '/antares-cse/sub--QOj3jY5QryarRgN'
+      }
+    }
     request(server)
     .post('/monitor')
     .set("access_token", token)
+    .send(body)
     .end((err, res) => {
       if(err){
         done(err)
